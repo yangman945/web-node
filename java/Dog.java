85555212c1712f3197c3079d0b4bae628fe375b7
yangman <yangman945@163.com>
2023-05-26 18:00:43 +0800

    feat: java变量类型

--- java/Dog.java
+++
@@ -1,8 +0,0 @@
-public class Dog {
-    public Dog(String name){
-        System.out.println("小狗的名字是："+name);
-    }
-    public static void main(String[] args){
-        Dog myDog = new Dog("汪汪汪");
-    }
-}
